de1e40506ad2c0f3a77b47b51b84d60f3616aaad
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2022-06-21 17:58:40 -0300

    resolução do requisito 3

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+/* eslint-disable react/destructuring-assignment */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Title extends Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>{headline}</h2>
+    );
+  }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
+
+export default Title;
